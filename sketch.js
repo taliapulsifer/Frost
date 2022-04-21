1cad0107022d8b517d37613e7da7ee54e5d1b326
Talia Pulsifer <70988138+taliapulsifer@users.noreply.github.com>
2022-04-20 22:21:06 -0400

    Working on the movement and angles

--- sketch.js
+++ sketch.js
@@ -1,81 +1,78 @@
 // Frost and Perlin Noise
+
+// global variables that are either final or need to be set once but available everywhere
 let maxLevel = 5;
 let branches = 2;
 let agl = Math.PI * 2 * 0.85
 let xOff = 0.0;
-
+let yOff = 0.0;
+let zOff = 0.0;
+let xStep = 0.01;
+let yStep = 0.02;
 function setup() {
   createCanvas(400, 400);
+  frameRate(60);
 }
 function draw() {
-  translate(200, 200);
   background(0);
-  // drawing in local space, after translation and rotation it is world space
-  let hex1 = new Hex(200, 200, 2);
-  let hex2 = new Hex(200, 200, 2);
-  hex1.draw();
-  push();
-  stroke(255, 0, 0);
-  strokeWeight(4);
-  /*point(hex1.getPoint(0).x, hex1.getPoint(0).y);
-  point(hex1.getPoint(2).x, hex1.getPoint(2).y);
-  point(hex1.getPoint(4).x, hex1.getPoint(4).y);*/
-  pop();
-  push();
-  //frostLine(0);
-  scale(0.5, 0.5);
-  //translate(200, 200);
-  for(let j = 0; j < 6; j++){
-    
-    let x = hex1.getPoint(j).x;
-    let y = hex1.getPoint(j).y;
+  // drawing in local space(using drawFrost function), after translation and rotation it is world space
+  //translate(200, 200); // intial translation moves to the center of the canvas
+  scale(0.5, 0.5); // scaling makes the image smaller, 0.5 makes it half of the size
+  drawFrost(3);// calling drawFrost, which uses the Hex class and calls frostLine
+}
+function drawFrost(int){
+  for(let j = 0; j < int; j++){
+    let hex = new Hex(200, 200, 2);
     push();
-    translate(x, y);
-    rotate(j * (60 * PI)/ 180)
-    frostLine(0);
+    translate(random(0, 400), random(0, 400));
+    hex.draw();
+    for(let j = 0; j < 6; j++){
+  
+      let x = hex.getPoint(j).x;
+      let y = hex.getPoint(j).y;
+      push();
+      translate(x, y);
+      rotate(j * (60 * PI)/ 180)
+      frostLine(0);
+      pop();
+    }
     pop();
   }
-  /*translate(-100, -200);
-  for(let j = 0; j < 6; j++){
-    
-    let x = hex2.getPoint(j).x;
-    let y = hex2.getPoint(j).y;
-    push();
-    translate(x, y);
-    rotate(j * (60 * PI)/ 180)
-    frostLine(0);
-    pop();
-  }*/
-  pop();
-
 }
+  // Using Perlin Noise to determine the angle that will be assigned to each particle
+  function angl(xOff){
+    xOff = xOff + 0.1;
+    let n = noise(xOff) * width;
+    return(n);
+  }
 
   function frostLine(level){
+    let a = 100;
     if (level > maxLevel){
       return
     }
     strokeWeight(1);
-    
+    agl = angl(xOff);
 
     for(let i = 0; i <= branches; i++){
       stroke(255);
-      line(0, 0, 150, 0);
-
+      line(0, 0, a, 0);
       push();
-      translate(200 * i / (branches + 1), 0);
+      translate(a * i / (branches + 1), 0);
       scale(0.5, 0.5);
 
       push();
-      rotate(angl(xOff));
+      rotate(agl);
       frostLine(level + 1);
       pop();
 
       push();
-      rotate(-angl(xOff));
+      rotate(-agl);
       frostLine(level + 1);
       pop();
 
       pop();
+      
     }
   }
 //Making hexagons a class so that I can access the points
@@ -92,6 +89,7 @@ class Hex {
   };
   }
   }
+  // Drawing the Hex
    draw() {
   for (let i = 0; i < 6; ++i) {
   line(
@@ -102,6 +100,7 @@ class Hex {
   );
   }
   }
+  // get point so that I can access each point on the Hex
    getPoint(index) {
    return {
     x : this.points[index].x,
@@ -109,10 +108,7 @@ class Hex {
     };
     }
   }
-  function angl(xOff){
-    xOff = xOff + 0.1;
-    let n = noise(xOff) * width;
-    return(n);
-  }
+
+
 
 
